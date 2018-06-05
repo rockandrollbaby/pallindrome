cf1ac7fa0d13f21ec2bbbe087fd3438e9daecb33
Cynthia Smith and Will Swanson <cynthia@>
2018-06-05 16:42:09 -0700

    try to get slice working but did not

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,27 @@ $(document).ready(function() {
     arrName = $("input#wordList").val().split("");
     function removeOdd(param1){
       if (arrName.length % 2 === 1){
-      var check = (((arrName.length-1)/2)+1)
-      delete arrName[check]
+      var check = ((arrName.length-1)/2)
+      alert(check);
+      param1.slice(check);
+      alert(param1);
     }
     return param1
   };
-  alert(arrName);
-    removeOdd(arrName);
+    arrName = removeOdd(arrName);
+    // alert(arrName);
     var full_length = (arrName.length);
     var half_length = (arrName.length/2);
     var leftSide = arrName.splice(0,half_length);
     var rightSide = arrName
-    console.log(leftSide.reverse().toString());
-    console.log(rightSide.toString());
-    debugger;
+    // console.log(leftSide.reverse().toString());
+    // console.log(rightSide.toString());
+    // debugger;
     if (leftSide.reverse().toString() === rightSide.toString()){
       alert("yay! it's a palindrome!")
     } else {
       alert("boooo, it's not a palindrome")
     }
-
     event.preventDefault();
   });
 });
